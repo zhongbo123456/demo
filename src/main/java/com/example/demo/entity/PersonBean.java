6b60bde0401db739abb1aea1079972fde19a1758
bo.zhong <bo.zhong@langyangtech.com>
2020-05-22 10:41:36 +0800

    af

---
+++ src/main/java/com/example/demo/entity/PersonBean.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+/**
+ * @BelongProjecet demo
+ * @BelongPackage com.example.demo.entity
+ * @Description:
+ * @Author: zhongbo
+ * @Date: 2020/5/20 15:11
+ */
+@Data
+public class PersonBean {
+    private String name;
+}
