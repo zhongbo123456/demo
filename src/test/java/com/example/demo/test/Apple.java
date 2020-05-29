788d07e8c8ed8c9a75974b1e50e0e5ca23b8d162
bo.zhong <bo.zhong@langyangtech.com>
2020-05-29 15:33:06 +0800

    stream 分组；数组输出

---
+++ src/test/java/com/example/demo/test/Apple.java
@@ -0,0 +1,36 @@
+package com.example.demo.test;
+
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
+
+/**
+ * @BelongProjecet demo
+ * @BelongPackage com.example.demo.test
+ * @Description:
+ * @Author: zhongbo
+ * @Date: 2020/5/28 15:12
+ */
+public class Apple {
+    private String name;
+    private Integer price;
+
+    public Apple(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
--- src/test/java/com/example/demo/test/Test18.java
+++ src/test/java/com/example/demo/test/Test18.java
@@ -1,10 +1,11 @@
 package com.example.demo.test;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @BelongProjecet demo
@@ -56,4 +57,72 @@ public class Test18 {
         }
 
     }
+
+    @Test
+    public void test4(){
+        Student student=new Student();
+        Object o = JSON.toJSON(student);
+        JSONObject jsonObject=new JSONObject();
+        if(o instanceof JSONObject){
+            jsonObject=(JSONObject)o;
+        }
+        boolean age = jsonObject.containsKey("age");
+        boolean id = jsonObject.containsKey("id");
+        System.out.println(id);
+        System.out.println(age);
+
+    }
+    @Test
+    public void test5(){
+        List<String> list=Arrays.asList("a","b","c");
+        List<Integer> list2=Arrays.asList(1,2,3,4);
+        List list1=list2;
+        for (Object o : list1) {
+            if(o instanceof String){
+                System.out.println("Stirng:"+o);
+            }
+            if(o instanceof Integer){
+                System.out.println("Integer:"+o);
+            }
+
+        }
+    }
+
+    /**
+     * stream 分组
+     */
+    @Test
+    public void test6(){
+        List<Apple> list=new ArrayList<>();
+        Apple apple1=new Apple("appleA",10);
+        Apple apple2=new Apple("appleA",45);
+        Apple apple3=new Apple("appleA",4);
+        Apple apple4=new Apple("appleB",22);
+        Apple apple5=new Apple("appleB",78);
+        Apple apple6=new Apple("appleC",89);
+        Apple apple7=new Apple("appleD",35);
+        Apple apple8=new Apple("appleE",38);
+        list.add(apple1);
+        list.add(apple2);
+        list.add(apple3);
+        list.add(apple4);
+        list.add(apple5);
+        list.add(apple6);
+        list.add(apple7);
+        list.add(apple8);
+        Map<String, Long> collect = list.stream().collect(Collectors.groupingBy(Apple::getName, Collectors.counting()));
+        System.out.println(collect.toString());
+
+    }
+
+    /**
+     * 数组输出Arrays.toString()
+     */
+    @Test
+    public void test7(){
+       int [] a={1,2,3,4,5,6};
+        String string = Arrays.toString(a);
+        System.out.println(string);
+    }
+
 }
